9cd10761c6b23add83ababc19bbe48f799b6d4ed
LeonPhillips <97657604+LeonPhillips@users.noreply.github.com>
2022-10-14 10:08:19 +1100

    Update vega_lite.js

--- vega_lite.js
+++ vega_lite.js
@@ -3,3 +3,21 @@ var vg_1 = "interactive_disp.json";
 vegaEmbed("#map_bar",vg_1).then(function(result) {
 
 }).catch(console.error)
+
+
+//2
+
+var vg_2 = "other_props.json";
+
+vegaEmbed("#other_props_id",vg_2).then(function(result) {
+
+}).catch(console.error)
+
+
+//3 
+
+var vg_3 = "time_comp.json";
+
+vegaEmbed("#time_comp_id",vg_3).then(function(result) {
+
+}).catch(console.error)
